b4483a1b5d735a25972a87eb5033766ea673e8a7
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-09-26 01:08:36 +0200

    Update app.py

--- applications/streamlit/image-classifier/app.py
+++ applications/streamlit/image-classifier/app.py
@@ -1,1 +1,79 @@
 import streamlit as st
+from PIL import Image 
+import matplotlib.pylab as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+model = tf.keras.Sequential([
+    hub.KerasLayer(
+        name='inception_v1',
+        handle='https://tfhub.dev/google/imagenet/inception_v1/classification/4',
+        trainable=False),
+])
+model.build([None, 224, 224, 3])
+
+def load_imagenet_labels(file_path):
+  labels_file = tf.keras.utils.get_file('ImageNetLabels.txt', file_path)
+  with open(labels_file) as reader:
+    f = reader.read()
+    labels = f.splitlines()
+  return np.array(labels)
+
+imagenet_labels = load_imagenet_labels('https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+
+def read_image(file_name):
+  image = tf.io.read_file(file_name)
+  image = tf.image.decode_jpeg(image, channels=3)
+  image = tf.image.convert_image_dtype(image, tf.float32)
+  image = tf.image.resize_with_pad(image, target_height=224, target_width=224)
+  return image
+
+img_url = {
+    'Fireboat': 'http://storage.googleapis.com/download.tensorflow.org/example_images/San_Francisco_fireboat_showing_off.jpg',
+    'Giant Panda': 'http://storage.googleapis.com/download.tensorflow.org/example_images/Giant_Panda_2.jpeg',
+}
+
+img_paths = {name: tf.keras.utils.get_file(name, url) for (name, url) in img_url.items()}
+img_name_tensors = {name: read_image(img_path) for (name, img_path) in img_paths.items()}img_url = {
+    'Fireboat': 'http://storage.googleapis.com/download.tensorflow.org/example_images/San_Francisco_fireboat_showing_off.jpg',
+    'Giant Panda': 'http://storage.googleapis.com/download.tensorflow.org/example_images/Giant_Panda_2.jpeg',
+}
+
+img_paths = {name: tf.keras.utils.get_file(name, url) for (name, url) in img_url.items()}
+img_name_tensors = {name: read_image(img_path) for (name, img_path) in img_paths.items()}
+
+plt.figure(figsize=(8, 8))
+for n, (name, img_tensors) in enumerate(img_name_tensors.items()):
+  ax = plt.subplot(1, 2, n+1)
+  ax.imshow(img_tensors)
+  ax.set_title(name)
+  ax.axis('off')
+plt.tight_layout()
+
+def top_k_predictions(img, k=3):
+  image_batch = tf.expand_dims(img, 0)
+  predictions = model(image_batch)
+  probs = tf.nn.softmax(predictions, axis=-1)
+  top_probs, top_idxs = tf.math.top_k(input=probs, k=k)
+  top_labels = imagenet_labels[tuple(top_idxs)]
+  return top_labels, top_probs[0]
+
+for (name, img_tensor) in img_name_tensors.items():
+  plt.imshow(img_tensor)
+  plt.title(name, fontweight='bold')
+  plt.axis('off')
+  plt.show()
+
+  pred_label, pred_prob = top_k_predictions(img_tensor)
+  for label, prob in zip(pred_label, pred_prob):
+    print(f'{label}: {prob:0.1%}')
+
+    
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+     st.write(file_details)
+  
+
+
